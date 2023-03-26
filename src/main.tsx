83b7c66d85bf732a1bf1cdefb3ee85f71e2a105e
Davvii1 <david2002rg@hotmail.com>
2023-03-26 11:37:54 -0600

    change router

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-])
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 )
